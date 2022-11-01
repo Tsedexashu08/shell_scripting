2ce146102547101bba31a19668f12662f4c2b58e
Tsedexashu08 <ashutsedex@gmail.com>
2022-11-01 02:19:17 -0900

    'advanced'

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,64 @@
+#!/usr/bin/p#!/usr/bin/python3
+"""gets top ten posts on subreddit """
+import json
+import requests
+
+
+def top_ten(subreddit):
+        """ gets the top 10 post of a subreddit"""
+            user_agent = {"User-Agent": "unix:0-subs.py:v1.0"}
+                data = requests.get("https://www.reddit.com/r/{}/hot/.json"
+                                        .format(subreddit),
+                                                                headers=user_agent,
+                                                                                        allow_redirects=False)
+                                                                                            if data.status_code != 200:
+                                                                                                    print("None")
+                                                                                                            return
+                                                                                                                json_data = data.json().get("data").get("children", [])
+                                                                                                                    if len(json_data) == 0:
+                                                                                                                            print("None")
+                                                                                                                                    return
+                                                                                                                                        for post in json_data[0:10]:
+                                                                                                                                                print(post.get("data").get("title"))
+""" gets top 10 post on subreddit """
+import json
+import requests
+
+
+def top_ten(subreddit):
+    """ gets the top 10 post of a subreddit"""
+    user_agent = {"User-Agent": "unix:0-subs.py:v1.0"}
+    data = requests.get("https://www.reddit.com/r/{}/hot/.json"
+                        .format(subreddit),
+                        headers=user_agent,
+                        allow_redirects=False)
+    if data.status_code != 200:
+        print("None")
+        return
+    json_data = data.json().get("data").get("children", [])
+    if len(json_data) == 0:
+        print("None")
+        return
+    for post in json_data[0:10]:
+        print(post.get("data").get("title"))ython3
+""" gets top 10 post on subreddit """
+import json
+import requests
+
+
+def top_ten(subreddit):
+    """ gets the top 10 post of a subreddit"""
+    user_agent = {"User-Agent": "unix:0-subs.py:v1.0"}
+    data = requests.get("https://www.reddit.com/r/{}/hot/.json"
+                        .format(subreddit),
+                        headers=user_agent,
+                        allow_redirects=False)
+    if data.status_code != 200:
+        print("None")
+        return
+    json_data = data.json().get("data").get("children", [])
+    if len(json_data) == 0:
+        print("None")
+        return
+    for post in json_data[0:10]:
+        print(post.get("data").get("title"))
