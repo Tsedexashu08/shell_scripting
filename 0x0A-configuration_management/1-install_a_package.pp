bc20f4d02fa4e7f389bb4087d4fe9323b5f0a17a
Tsedexashu08 <ashutsedex@gmail.com>
2022-12-21 04:53:49 -0900

    'commit'

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# Using Puppet, install flask from pip3 
-package { 'flask':  
-  ensure   => '2.1.0',   
-  provider => 'pip3', 
-  }
+#using instaler package
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
